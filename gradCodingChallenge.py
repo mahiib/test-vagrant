448fe4de95b50b95ea44654c04423900aaae7faa
Mahesh B <61156481+mahiib@users.noreply.github.com>
2022-12-17 09:58:22 +0530

    Update gradCodingChallenge.py

--- gradCodingChallenge.py
+++ gradCodingChallenge.py
@@ -1,7 +1,7 @@
 #Getting user's weekly budget
 budget=int(input())
 
-#dictionary to stores weekly costs of newspapers
+#Dictionary to stores weekly costs of newspapers
 newspaperCost={}
 
 
@@ -11,15 +11,20 @@ newspaperCost['ET']=34
 newspaperCost['BM']=10.5
 newspaperCost['HT']=18
 
-#Converting 
+#Converting dictionary to list
 newspaperCostList = list(newspaperCost)
 
+
+
+#Stores all pairs of combinations
+res = [(x, y) for idx, x in enumerate(newspaperCostList) for y in newspaperCostList[idx + 1: ]]
+
+#Stores all possible combinations
 possibleSubscriptions=[]
 
-#Stores all pairs of combinations among the 
-res = [(x, y) for idx, x in enumerate(newspaperCostList) for y in newspaperCostList[idx + 1: ]]
 for (x,y) in res:
     if newspaperCost[x]+newspaperCost[y]<budget:
         possiblePairs='{"'+x+'"'+',"'+y+'"}'
         possibleSubscriptions.append(possiblePairs)
+
 print(possibleSubscriptions)
